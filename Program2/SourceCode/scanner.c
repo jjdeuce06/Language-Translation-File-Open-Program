f88ddd00f982d2cc2620dbf7613ac7329fa63917
John Gerega <jjgerega@gmail.com>
2026-02-11 14:59:33 -0500

    Added some code, see if you can get it to run

--- Program2/SourceCode/scanner.c
+++ Program2/SourceCode/scanner.c
@@ -8,6 +8,7 @@ Token scanner(char *buffer, FILE *in_file, FILE *out_file, FILE *list_file)
 {
     char ch;
     Token ans;
+    char errors[128];
 
     clear_buffer(buffer);
 
@@ -92,8 +93,40 @@ Token scan_digits(char *buffer, FILE *in_file)
     return INTLITERAL;
 }
 
-void clear_buffer(char *){}
-Token check_reserved(char *){}
-int lexical_error(char *, int flag, FILE *){}
-void token_ident(Token, char *){}
-void add_char(char *, char){}
+void clear_buffer(char *buffer)
+{
+    printf("clearing buffer: %s", buffer);
+    buffer[0] = '\0';
+    printf("buffer after clear: %s", buffer);
+}
+
+//returns the token of a string
+Token check_reserved(char *buffer)
+{
+    printf("checking if reserved: %s\n", buffer);
+}
+//add character that caused error to an array
+int lexical_error(char *buffer, int flag, FILE *list_file)
+{
+    if (flag == 0)
+    {
+        //should go to the listing file and print the error message there, not the console
+        printf("Lexical Error: '%s' not recognized\n", buffer);
+    }
+    
+}
+
+//meant to identify the string for the given token
+void token_ident(Token token, char *buffer)
+{
+    printf("Token: %d, Buffer: %s\n", token, buffer);
+    
+}
+
+//adds a character to the buffer
+void add_char(char *buffer, char ch)
+{
+    int len = strlen(buffer);
+    buffer[len] = ch;
+    buffer[len + 1] = '\0';
+}
